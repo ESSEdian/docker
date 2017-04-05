f36ce1bcd066b9439492551e12209c503689a6c9
lake <102535481@qq.com>
2017-04-05 18:25:04 +0800

    a

--- rancher-create.sh
+++ rancher-create.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d --restart=unless-stopped -p 8080:8080 -v /root/storage/rancher/db:/var/lib/mysql -e JAVA_OPTS="-Xms512m -Xmx1500m" rancher/server
+docker run -d --restart=unless-stopped -p 8080:8080 -v /root/storage/rancher/db:/var/lib/mysql -e JAVA_OPTS="-Xms512m -Xmx1024m" rancher/server
