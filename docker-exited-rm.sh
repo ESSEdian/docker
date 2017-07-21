773c0fad128e20499cd30138f51cfbfef9bc5789
lake <102535481@qq.com>
2017-07-21 02:34:15 +0000

    a

--- docker-exited-rm.sh
+++ docker-exited-rm.sh
@@ -3,3 +3,7 @@ for id in `docker ps -a --filter exited=137 --format "{{.ID}}"`
 do
 	docker rm $id	
 done
+for id in `docker ps -a --filter status=created --format "{{.ID}}"`
+do
+	docker rm $id	
+done
