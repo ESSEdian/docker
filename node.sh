c6f1bfcf6f03d3a5d92203964bda55dce67bfb91
root <root@1.host.bjike.com>
2017-08-31 17:46:32 +0800

    update

--- node.sh
+++ node.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 lasLine=`awk 'NF{a=$0}END{print a}' /root/.bashrc`
-echo "alias status=\"/root/issp/docker/node/status.sh\"" >> /root/.bashrc
-echo "alias stop=\"/root/issp/docker/node/stop.sh\"" >> /root/.bashrc
-echo "alias stop-kill=\"/root/issp/docker/node/stop-kill.sh\"" >> /root/.bashrc
-echo "alias restart=\"/root/issp/docker/node/restart.sh\"" >> /root/.bashrc
-echo "alias deploy=\"/root/issp/docker/node/run.sh\"" >> /root/.bashrc
-echo "alias log=\"/root/issp/docker/node/log.sh\"" >> /root/.bashrc
-echo "alias clean-log=\"/root/issp/docker/node/clean-log.sh\"" >> /root/.bashrc
-echo "alias h=\"/root/issp/docker/node/help.sh\"" >> /root/.bashrc
-source /root/.bashrc
+if [ $lasLine != "alias h=\"/root/issp/docker/node/help.sh\"" ];then
+	echo "alias status=\"/root/issp/docker/node/status.sh\"" >> /root/.bashrc
+	echo "alias stop=\"/root/issp/docker/node/stop.sh\"" >> /root/.bashrc
+	echo "alias stop-kill=\"/root/issp/docker/node/stop-kill.sh\"" >> /root/.bashrc
+	echo "alias restart=\"/root/issp/docker/node/restart.sh\"" >> /root/.bashrc
+	echo "alias deploy=\"/root/issp/docker/node/run.sh\"" >> /root/.bashrc
+	echo "alias log=\"/root/issp/docker/node/log.sh\"" >> /root/.bashrc
+	echo "alias clean-log=\"/root/issp/docker/node/clean-log.sh\"" >> /root/.bashrc
+	echo "alias h=\"/root/issp/docker/node/help.sh\"" >> /root/.bashrc
+	source /root/.bashrc
+fi
 /root/issp/gitlab-webhook/start.sh ; /root/issp/docker/node/run.sh
 tail -f /var/log/node.log
