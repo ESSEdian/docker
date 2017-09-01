07b6c93b384ff5baa34af9bed3b217ec7db21237
huanghuanlai <102535481@qq.com>
2017-09-01 11:13:30 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-lasLine=`awk 'NF{a=$0}END{print a}' /root/.bashrc`
-if [ $lasLine != "alias h=\"/root/issp/docker/java/help.sh\"" ];then
-	echo "alias status=\"/root/issp/docker/java/status.sh\"" >> /root/.bashrc
-	echo "alias stop=\"/root/issp/docker/java/stop.sh\"" >> /root/.bashrc
-	echo "alias stop-kill=\"/root/issp/docker/java/stop-kill.sh\"" >> /root/.bashrc
-	echo "alias restart=\"/root/issp/docker/java/restart.sh\"" >> /root/.bashrc
-	echo "alias deploy=\"/root/issp/docker/java/run.sh\"" >> /root/.bashrc
-	echo "alias log=\"/root/issp/docker/java/log.sh\"" >> /root/.bashrc
-	echo "alias clean-log=\"/root/issp/docker/java/clean-log.sh\"" >> /root/.bashrc
-	echo "alias h=\"/root/issp/docker/java/help.sh\"" >> /root/.bashrc
-	source /root/.bashrc
+if [ ! -f /usr/bin/h ];then
+	cp -rf /root/issp/docker/java/bin/* /usr/bin
 fi
 /root/issp/gitlab-webhook/start.sh ; /root/issp/docker/java/run.sh
 tail -f /var/log/java.log
--- node.sh
+++ node.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-lasLine=`awk 'NF{a=$0}END{print a}' /root/.bashrc`
-if [ $lasLine != "alias h=\"/root/issp/docker/node/help.sh\"" ];then
-	echo "alias status=\"/root/issp/docker/node/status.sh\"" >> /root/.bashrc
-	echo "alias stop=\"/root/issp/docker/node/stop.sh\"" >> /root/.bashrc
-	echo "alias stop-kill=\"/root/issp/docker/node/stop-kill.sh\"" >> /root/.bashrc
-	echo "alias restart=\"/root/issp/docker/node/restart.sh\"" >> /root/.bashrc
-	echo "alias deploy=\"/root/issp/docker/node/run.sh\"" >> /root/.bashrc
-	echo "alias log=\"/root/issp/docker/node/log.sh\"" >> /root/.bashrc
-	echo "alias clean-log=\"/root/issp/docker/node/clean-log.sh\"" >> /root/.bashrc
-	echo "alias h=\"/root/issp/docker/node/help.sh\"" >> /root/.bashrc
-	source /root/.bashrc
+if [ ! -f /usr/bin/h ];then
+	cp -rf /root/issp/docker/node/bin/* /usr/bin
 fi
 /root/issp/gitlab-webhook/start.sh ; /root/issp/docker/node/run.sh
 tail -f /var/log/node.log
