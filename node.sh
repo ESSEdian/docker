3d1963fa0846261bb6b20aeb80b9a70f036e00f7
huanghuanlai <102535481@qq.com>
2017-09-02 17:05:02 +0800

    udpate

--- node.sh
+++ node.sh
@@ -2,6 +2,7 @@
 if [ ! -f /usr/bin/h ];then
 	cp -rf /root/issp/docker/node/bin/* /usr/bin
 	cp -rf /root/issp/docker/node/node.sh /usr/bin/
+	rm -rf /usr/bin/node.sh
 	sed -i '$d' /usr/bin/node.sh
 #projectPshellPre是在系统环境变量里面读取
 	echo "$projectPshellPre node index.js &" >> /usr/bin/node.sh
