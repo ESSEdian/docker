a65a964af041ec9cedd7ad16ecbafd57725283ba
lake <102535481@qq.com>
2017-11-27 18:19:52 +0800

    增加jvm远程调优配置

--- java/java.sh
+++ java/java.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 cd /$project
-java -jar -Xms400m -Xmx400m $projectPshellPre `ls | grep "jar$"` $projectPshellPos &
+java \ 
+ -Dcom.sun.management.jmxremote.authenticate=true \
+ -Dcom.sun.management.jmxremote.port=3333 \
+ -Dcom.sun.management.jmxremote.access.file=jmxremote.access \
+ -Dcom.sun.management.jmxremote.password.file=jmxremote.password \
+ -Dcom.sun.management.jmxremote.ssl=false \
+ -Djava.rmi.server.hostname=test.domain.dounine.com \
+ -jar -Xms400m -Xmx400m $projectPshellPre `ls | grep "jar$"` $projectPshellPos &
