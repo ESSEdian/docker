5f111a6ce0833d745d9aac9b2cf4a75ecf231912
lake <102535481@qq.com>
2017-11-28 12:16:55 +0800

    jmx

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 if [ ! -f /usr/bin/h ]
 then
-	echo $jvm_jmx_username" "$jvm_jmx_password >> /etc/jvm.jmx.password
-	chmod 600 /etc/jvm.jmx.password
-	if [ "$jvm_jmx_role" == "" ]; then
-			jvm_jmx_role="readonly"
+	
+	if [ "$jvm_jmx_remote" == "true" ];then
+		echo $jvm_jmx_username" "$jvm_jmx_password >> /etc/jvm.jmx.password
+		chmod 600 /etc/jvm.jmx.password
+		if [ "$jvm_jmx_role" == "" ]; then
+				jvm_jmx_role="readonly"
+				#readwrite
+		fi
+		echo $jvm_jmx_username" "$jvm_jmx_role >> /etc/jvm.jmx.access
+		chmod 600 /etc/jvm.jmx.access
 	fi
-	echo $jvm_jmx_username" "$jvm_jmx_role >> /etc/jvm.jmx.access
-	chmod 600 /etc/jvm.jmx.access
-
+	
 	ln -s /root/issp/docker/java/clean-log.sh /usr/bin/clean-log
 	ln -s /root/issp/docker/java/run.sh /usr/bin/deploy
 	ln -s /root/issp/docker/java/help.sh /usr/bin/h
--- java/java.sh
+++ java/java.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /$project
-if [ "$jvm_jmx" == "true" ];then
+if [ "$jvm_jmx_remote" == "true" ];then
 	java -Dcom.sun.management.jmxremote.authenticate=true -Dcom.sun.management.jmxremote.port=$jvm_jmx_port -Dcom.sun.management.jmxremote.access.file=/etc/jvm.jmx.access -Dcom.sun.management.jmxremote.password.file=/etc/jvm.jmx.password -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=$jvm_jmx_hostname -jar -Xms"$jvm_Xms" -Xmx"$jvm_Xmx" $projectPshellPre `ls | grep "jar$"` $projectPshellPos &
 else
 	java -jar -Xms"$jvm_Xms" -Xmx"$jvm_Xmx" $projectPshellPre `ls | grep "jar$"` $projectPshellPos &
